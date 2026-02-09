61f0fd8bc31044b9a014815858f3d32462cbc11c
Zuzanna Gajewska <zgajewska19@gmail.com>
2026-02-09 22:06:17 +0100

    dodawanie

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Data;
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -9,6 +11,15 @@
         Console.WriteLine("Podaj nr telefonu: ");
         int phone_number = int.Parse(Console.ReadLine());
 
+        AddToFile(name, surname, phone_number);
+    }
+
+    public static void AddToFile(string name, string surname, int phone_number)
+    {
+        using StreamWriter writer = new StreamWriter("dane.txt", append: true);
+
+        writer.WriteLine($"imie: {name}, nazwisko: {surname}, nr_telefonu: {phone_number}");
+        writer.Close();
     }
 
 }
