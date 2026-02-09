36afa45e34b4faa80e7ed81494c7d6fcee0fd6cc
Zuzanna Gajewska <zgajewska19@gmail.com>
2026-02-09 21:35:37 +0100

    dane

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj imię: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Podaj nazwisko: ");
+        string surname = Console.ReadLine();
+        Console.WriteLine("Podaj nr telefonu: ");
+        int phone_number = int.Parse(Console.ReadLine());
+
+    }
+
+}
